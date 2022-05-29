8d551c62e48dbeb4eeb2d9de41ef4fb8ee6294cc
aponmahmud1999 <aponmahmud36@gmail.com>
2022-05-29 21:32:53 +0600

    users and products code done

--- index.js
+++ index.js
@@ -14,15 +14,71 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: "UnAuthorized access" })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
+
 async function run(){
     try{
 
         await client.connect();
         const productCollection = client.db("cars-parts").collection("products")
+        const userCollection = client.db("electronics-lab").collection("users")
+
+
+
+        const verifyAdmin = async (req, res, next) => {
+            const initiator = req.decoded.email
+            const initiatorAcc = await userCollection.findOne({ email: initiator })
+            if (initiatorAcc.role === 'admin') {
+                next()
+            }
+            else {
+                return res.status(403).send({ message: "Forbidden access" })
+            }
+        }
 
         app.get('/product', async (req, res) => {
             const products = await productCollection.find().toArray()
             res.send(products)
+        })
+        app.post('/product', verifyJWT, verifyAdmin, async (req, res) => {
+            const product = req.body
+            const products = await productCollection.insertOne(product)
+            res.send(products)
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const quantity = req.body.newQuantity
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: { quantity: quantity },
+            };
+            const product = await productCollection.updateOne(filter, updateDoc)
+            res.send(product)
+        })
+        app.delete('/product/:_id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params._id
+            const filter = { _id: ObjectId(id) }
+            const result = await productCollection.deleteOne(filter)
+            res.send(result)
         })
 
     }
